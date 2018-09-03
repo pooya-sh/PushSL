51696c1cd8908c1b477f07ca1a66d19d88b1b8ce
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-09-03 17:53:38 +0200

    Add trip layout and separate HTML modules

---
+++ src/main/java/com/pushsl/pushsl/Objects/Test.java
@@ -0,0 +1,16 @@
+package com.pushsl.pushsl.Objects;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Test {
+
+    @GetMapping("/")
+    public ModelAndView test(){
+        int[] array = new int [] {1,2,3,4};
+        return  new ModelAndView("index")
+                .addObject("array", array);
+    }
+}
