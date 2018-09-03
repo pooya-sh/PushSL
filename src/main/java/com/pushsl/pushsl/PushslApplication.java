365647f1028a9e51c58b639382c3d8bffe95e6f4
Ludwig Hedeby <ludwig@hedeby.me>
2018-09-03 11:58:05 +0200

    Initialize spring project

---
+++ src/main/java/com/pushsl/pushsl/PushslApplication.java
@@ -0,0 +1,12 @@
+package com.pushsl.pushsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushslApplication.class, args);
+    }
+}
