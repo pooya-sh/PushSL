2f4c9204f625818954f1df4b4ee69825a899c8bd
Ludwig Hedeby <ludwig@hedeby.me>
2018-09-04 14:07:53 +0200

    Add rest controller

---
+++ src/main/java/com/pushsl/pushsl/WebRestController.java
@@ -0,0 +1,22 @@
+package com.pushsl.pushsl;
+
+import com.pushsl.pushsl.Objects.SiteInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WebRestController {
+
+    @Autowired
+    APIData apiData;
+
+    @PostMapping("/siteinfo")
+    public List<SiteInfo> getSiteInfo(@RequestBody String searchString) {
+        return apiData.getSiteInfo(searchString);
+    }
+
+}
