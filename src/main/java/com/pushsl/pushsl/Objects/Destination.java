c3fba14022c07baa0c3a1f23b707bf3fe0afa71e
Ludwig Hedeby <ludwig@hedeby.me>
2018-09-04 14:52:08 +0200

    Add controller for trip planner

--- src/main/java/com/pushsl/pushsl/Objects/Destination.java
+++ src/main/java/com/pushsl/pushsl/Objects/Destination.java
@@ -2,11 +2,19 @@ package com.pushsl.pushsl.Objects;
 
 public class Destination {
     public String name;
+    public String time;
+    public String date;
+    public String rtTime;
+    public String rtDate;
 
     @Override
     public String toString() {
         return "Destination{" +
                 "name='" + name + '\'' +
+                ", time='" + time + '\'' +
+                ", date='" + date + '\'' +
+                ", rtTime='" + rtTime + '\'' +
+                ", rtDate='" + rtDate + '\'' +
                 '}';
     }
 }
