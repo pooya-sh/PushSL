1059c6b9d2f85f1f386152990b08e87d1ea6e693
Administrator <emmaljungdahl@gmail.com>
2018-09-07 16:44:48 +0200

    Added function to input fields and the counter

--- src/main/java/com/pushsl/pushsl/WebRestController.java
+++ src/main/java/com/pushsl/pushsl/WebRestController.java
@@ -56,6 +56,5 @@ public class WebRestController {
             System.out.println("less than 30 minutes left, fetching real time");
             return apiData.getRemainingTime(trip);
         }
-
     }
 }
