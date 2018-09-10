f52faa1b956371de109cab548eaedd675d4bdf16
Ludwig Hedeby <ludwig@hedeby.me>
2018-09-10 10:37:59 +0200

    Add siteinfo parameter to addData method

--- src/main/java/com/pushsl/pushsl/Repository.java
+++ src/main/java/com/pushsl/pushsl/Repository.java
@@ -45,7 +45,7 @@ public class Repository {
         }
     }
 
-    public void addData(String email, String journeyNumberRT, String timeBeforeLeaving) throws SQLException {
+    public void addData(String email, String journeyNumberRT, String timeBeforeLeaving, String siteId) throws SQLException {
         try (Connection conn = dataSource.getConnection()) {
             PreparedStatement checkUsers = conn.prepareStatement("SELECT * FROM Users WHERE UserEmail = ? ");
             checkUsers.setString(1, email);
@@ -65,11 +65,12 @@ public class Repository {
                 id = resultSet.getInt("User_ID");
             }
 
-            PreparedStatement addTravelStatemnet = conn.prepareStatement("INSERT INTO Travels VALUES (?,?,?,?)");
+            PreparedStatement addTravelStatemnet = conn.prepareStatement("INSERT INTO Travels VALUES (?,?,?,?,?)");
             addTravelStatemnet.setInt(1, Integer.parseInt(journeyNumberRT));
             addTravelStatemnet.setString(2, timeBeforeLeaving);
             addTravelStatemnet.setString(3, "1956-03-11");
             addTravelStatemnet.setInt(4, id);
+            addTravelStatemnet.setString(5, siteId);
             addTravelStatemnet.executeUpdate();
 
             System.out.println("id: " + id);
